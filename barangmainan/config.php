d78caeb098653666ead7fe056348030f60cb8d15
nurfatinnblh14 <47810583+nurfatinnblh14@users.noreply.github.com>
2019-04-16 12:58:01 +0800

    Add files via upload

---
+++ barangmainan/config.php
@@ -0,0 +1,7 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'KedaiDuluDulu';
+$databaseUsername = 'root';
+$databasePassword = '';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername,$databasePassword, $databaseName );
+?>
